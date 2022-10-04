079d151342c731fc3a548ddc17e449dd6b762137
VikaAkhapkina <akhapkina_viktoriya@mail.ru>
2022-10-04 16:40:18 +0300

    added TextField + saveButten

---
+++ PersonNavig/PersonNavig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonNavig
+//
+//  Created by Viktoriya on 4.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+      
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.placeholder
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveButten(_ sender: Any) {
+    }
+    
+}
+
